10e50cc90b6e97839413afeb0c64cce9e6fd5d85
Jonas Boutelhik <jonasbo42@gmail.com>
2022-01-27 17:53:06 +0100

    this should fix the price alerts

--- src/components/PriceAlertWatcher.js
+++ src/components/PriceAlertWatcher.js
@@ -1,4 +1,4 @@
-import { isBefore, sub } from 'date-fns'
+import { isAfter, sub } from 'date-fns'
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { newPriceAlert } from '../features/pricealerts/priceAlertsSlice'
@@ -23,10 +23,11 @@ const PriceAlertWatcher = () => {
         let priceChange24h = asset.price_change_percentage_24h
         if (Math.abs(priceChange24h) > MAX_PRICE_CHANGE_24H) {
           let now = new Date()
-          let yesterday = sub(now, {day: 1})
-          // check if a price alert for this asset has already been triggered
-          let oldAlert = priceAlerts.find(e => {
-            return e.asset_id === asset.id && e.days === 1 && isBefore(new Date(e.date), yesterday)
+          let yesterday = sub(now, {days: 1})
+          let currentAssetPriceAlerts = priceAlerts.filter(e => e.asset_id === asset.id && e.days === 1)
+          // was there a price alert for this asset in the last 24 hours?
+          let oldAlert = currentAssetPriceAlerts.find(e => {
+            return isAfter(new Date(e.date), yesterday)
           })
           if (oldAlert === undefined) {
             let alert = {
@@ -34,7 +35,7 @@ const PriceAlertWatcher = () => {
               asset_id: asset.id,
               asset_name: asset.name,
               price_increased: Math.sign(priceChange24h) === 1 ? true : false,
-              text: Math.sign(priceChange24h) === 1 ? `Price increased by more than ${MAX_PRICE_CHANGE_24H}% in the last 24 hours!` : `Price fell by more than ${MAX_PRICE_CHANGE_24H}% in the last 24 hours!`,
+              price_change: priceChange24h,
               date: new Date().toISOString(),
               days: 1, // time period
               hidden: false
@@ -47,10 +48,11 @@ const PriceAlertWatcher = () => {
         let priceChange7days = (sparklinePrices[sparklinePrices.length-1] - sparklinePrices[0]) / asset.current_price * 100
         if (Math.abs(priceChange7days) > MAX_PRICE_CHANGE_7D) {
           let now = new Date()
-          let lastWeek = sub(now, {day: 7})
-          // check if a price alert for this asset has already been triggered
-          let oldAlert = priceAlerts.find(e => {
-            return e.asset_id === asset.id && e.days === 7 && isBefore(new Date(e.date), lastWeek)
+          let lastWeek = sub(now, {days: 7})
+          let currentAssetPriceAlerts = priceAlerts.filter(e => e.asset_id === asset.id && e.days === 7)
+          // was there a price alert for this asset in the last 7 days?
+          let oldAlert = currentAssetPriceAlerts.find(e => {
+            return isAfter(new Date(e.date), lastWeek)
           })
           if (oldAlert === undefined) {
             let alert = {
@@ -58,7 +60,7 @@ const PriceAlertWatcher = () => {
               asset_id: asset.id,
               asset_name: asset.name,
               price_increased: Math.sign(priceChange7days) === 1 ? true : false,
-              text: Math.sign(priceChange7days) === 1 ? `Price increased by more than ${MAX_PRICE_CHANGE_7D}% in the last 7 days!` : `Price fell by more than ${MAX_PRICE_CHANGE_7D}% in the last 7 days!`,
+              price_change: priceChange7days,
               date: new Date().toISOString(),
               days: 7, // time period
               hidden: false
